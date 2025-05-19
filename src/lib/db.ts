e019758573697b59bd978d8244f93a492aded162
GustavoTZ14 <gterrin155@gmail.com>
2025-05-18 21:16:27 -0300

    up2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,10 +4,10 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST!,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_DATABASE!,
+  host: 'localhost',
+  user: 'gustavotz',
+  password: 'G78u45s21tA$',
+  database: 'receitas',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
